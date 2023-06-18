df2380d4d6bb7175250a160dba711cc201ca3921
Dmitry Mamchenko <dmitry.mamchenko@gmail.com>
2023-06-18 17:28:21 +0300

    Calc

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,132 @@
-import logo from './logo.svg';
-import { createElement } from 'react';
-import { currentDate } from './date';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
 
-const render = () => {
-    let date = createElement('div', { className: 'date' }, currentDate());
-    let link = createElement(
-        'a',
-        {
-            className: 'App-link',
-            href: 'https://reactjs.org',
-            target: '_blank',
-            rel: 'noopener noreferrer',
-        },
-        'Learn React',
-    );
-
-    const text = createElement(
-        'p',
-        null,
-        'Edit',
-        createElement('code', null, 'src/App.js'),
-        'and save to reload.',
-    );
-
-    return createElement(
-        'div',
-        { className: 'App' },
-        createElement(
-            'header',
-            { className: 'App-header' },
-            createElement('img', {
-                className: 'App-logo',
-                src: logo,
-                alt: 'logo',
-            }),
-            text,
-            link,
-            date,
-        ),
-    );
-};
+const render = () => {};
 
 export const App = () => {
-    return render();
+    const [numberBtns, setNumberBtns] = useState([
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+    ]);
+
+    const actionBtns = ['+', '-', '=', 'reset'];
+
+    const [operator, setOperator] = useState('');
+    const [value, setValue] = useState('0');
+    const [value1, setValue1] = useState('');
+    const [finish, setFinish] = useState(false);
+
+    let showResult = value + operator + value1;
+
+    const handleNumber = (number) => {
+        if (operator) {
+            setValue1((prev) => prev + number);
+        } else {
+            setValue((prevValue) => {
+                if (prevValue === '0' || number === '0') {
+                    return number;
+                } else {
+                    return prevValue + number;
+                }
+            });
+        }
+    };
+
+    const handleAction = (actionType) => {
+        switch (actionType) {
+            case 'reset':
+                setValue('0');
+                setValue1('');
+                setOperator('');
+                break;
+            case '=':
+                let res = '0';
+                switch (operator) {
+                    case '-':
+                        res = +value - +value1;
+                        setFinish(true);
+                        break;
+                    case '+':
+                        res = +value + +value1;
+                        setFinish(true);
+                        break;
+                    default:
+                        break;
+                }
+                setValue(res);
+                setValue1('');
+                setOperator('');
+                //TODO как то попроще тут можно?
+                break;
+            default:
+                setOperator(actionType);
+                setFinish(false);
+                break;
+        }
+    };
+
+    const createBtnAction = (btn) => {
+        let name = btn;
+        let classBtn = [styles.calcBtn];
+        if (btn === '=' || btn === 'reset') {
+            classBtn.push(styles.calcBtnFull);
+        }
+        if (btn === 'reset') {
+            name = 'Сброс';
+        }
+        return (
+            <div
+                className={classBtn.join(' ')}
+                key={btn}
+                onClick={() => {
+                    handleAction(btn);
+                }}
+            >
+                {name}
+            </div>
+        );
+    };
+
+    return (
+        <div className={styles.calc}>
+            <div className={styles.calcWrapper}>
+                {console.log(showResult)}
+                <div
+                    className={
+                        finish ? styles.calcDisplayResult : styles.calcDisplay
+                    }
+                >
+                    {showResult}
+                </div>
+                <div className={styles.calcBtnsWrapper}>
+                    <div className={styles.calcNumbers}>
+                        {numberBtns.map((number) => (
+                            <div
+                                className={[
+                                    styles.calcNumber,
+                                    styles.calcBtn,
+                                ].join(' ')}
+                                key={number}
+                                onClick={() => {
+                                    setFinish(false);
+                                    handleNumber(number);
+                                }}
+                            >
+                                {number}
+                            </div>
+                        ))}
+                        {actionBtns.map((btn) => createBtnAction(btn))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
---
+++ my-app/src/CalcComponent.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+
+export const CalcComponent = () => {
+
+    return console.log('1231');
+}
